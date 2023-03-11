ed801c28b3628db49b50ede43b74c98e3f9f953f
lazevedo <lazevedo@STFSAOC045860-L>
2023-03-11 17:14:58 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+
+        static string calculaMediaAritmetica(string notaValor01, string notaValor02)
+
+        {
+
+            const int valorPonderado = 7; int mediaAritmetica = 0;
+
+
+
+            mediaAritmetica = (int.Parse(notaValor01) + int.Parse(notaValor02) / int.Parse("2"));
+
+
+
+            var resultadoFinal = (mediaAritmetica >= valorPonderado) ? "Aprovado" : "Reprovado";
+
+
+
+            return resultadoFinal;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" ### Programa Média Aritmética  ### ");
+
+            string n1 = "8";
+
+            string n2 = "9";
+
+            string msgValida = calculaMediaAritmetica(n1, n2);
+
+
+
+            Console.WriteLine("Média Final >> " + msgValida);
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ ConsoleApp_Notas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp_Notas
+{
+    class Program
+    {
+
+        static string calculaMediaAritmetica(int notaValor01, int notaValor02)
+        {
+            const int valorPonderado = 7; int mediaAritmetica = 0;
+
+            mediaAritmetica = (notaValor01 + notaValor02) / 2;
+
+            var resultadoFinal = (mediaAritmetica >= valorPonderado) ? "Aprovado" : "Reprovado";
+
+            return resultadoFinal;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ### Programa Média Aritmética  ### ");
+
+            string msgValida = calculaMediaAritmetica(8, 9).ToString();
+
+            Console.WriteLine("Média Final >> " + msgValida);
+            Console.ReadKey();
+        }
+    }
+}
